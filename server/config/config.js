5656514fd9901b839331c266cbae2059450b7ed1
Petyo Arhangelov <73829572+Arhangelov@users.noreply.github.com>
2021-10-01 22:21:39 +0300

    Delete server/config directory

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    PORT: 5001,
-    DB_URI: 'mongodb+srv://Arhangelov:Ss546213827913@taishicluster.klsgn.mongodb.net/taishiDB?retryWrites=true&w=majority',
-    SALT_ROUNDS: 10,
-    SECRET: 'HYPERSECRET',
-    COOKIE_NAME: 'session'
-};
-
-module.exports = config;
